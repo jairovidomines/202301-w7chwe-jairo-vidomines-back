7c5870676ce556a88703ac89acb27f7ccfca770a
Jairo Vidomines <jvidomines@gmail.com>
2023-02-24 21:21:31 +0100

    Create connectDatabase

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import "./loadEnvironment.js";
+import connectDatabase from "./database/connectDatabase.js";
 import { startServer } from "./server/startServer.js";
 
 const port = process.env.PORT ?? 8001;
+const mongoUrl = process.env.MONGODB_CONNECTION_URL!;
 
+await connectDatabase(mongoUrl);
 await startServer(+port);
