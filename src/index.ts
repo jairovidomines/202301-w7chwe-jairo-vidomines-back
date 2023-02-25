fabc0a938727cc6dd9bb129a214cb62d434ed568
Jairo Vidomines <jvidomines@gmail.com>
2023-02-25 09:50:24 +0100

    Create CustomError

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "./loadEnvironment.js";
 import connectDatabase from "./database/connectDatabase.js";
 import { startServer } from "./server/startServer.js";
 
-const port = process.env.PORT ?? 8001;
+export const port = process.env.PORT ?? 8001;
 const mongoUrl = process.env.MONGODB_CONNECTION_URL!;
 
 await connectDatabase(mongoUrl);
